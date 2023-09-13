9df75662d1b45d48f292d9c95efa08ba576f0abb
Md Fahad kabir <fahadkabirsezan@gmail.com>
2023-09-13 15:13:04 +0600

    gym

--- src/app/my-first-comp/my-first-comp.component.ts
+++ src/app/my-first-comp/my-first-comp.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-my-first-comp',
   templateUrl: './my-first-comp.component.html',
-  styleUrls: ['./my-first-comp.component.scss']
+  styleUrls: ['./my-first-comp.component.scss'],
 })
 export class MyFirstCompComponent {
+  name: string = '';
+  email: string = '';
+  message: string = '';
+  isSubmitted: boolean = false;
+  messages: Array<any> = [];
 
+  // method
+  onSubmit(): void {
+    this.isSubmitted = true;
+    this.messages.push({
+      name: this.name,
+      email: this.email,
+      message: this.message,
+    });
+    console.log(this.messages);
+    // console.log(this.name);
+  }
 }
